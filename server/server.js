9c28b9239cb974a2e4f4c0bf7c0425a5488a6ad5
DashkevichAS <antonsnov0604@gmail.com>
2022-12-28 13:10:29 +0300

    CRUD posts

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-import { registerValidation } from './validations/auth.js';
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import { getMe, login, register } from './controllers/UserController.js';
+import {
+  createPost,
+  getAllPosts,
+  getOnePost,
+  deletePost,
+  updatePost,
+} from './controllers/PostController.js';
 
 mongoose
   .connect(
@@ -13,16 +24,19 @@ mongoose
   .catch((error) => console.log('Mongo error:', error));
 
 const app = express();
+app.use(express.json());
 
 const PORT = 4444;
 
-app.use(express.json());
-
 app.post('/auth/registration', registerValidation, register);
-
-app.post('/auth/login', login);
-
+app.post('/auth/login', loginValidation, login);
 app.get('/auth/me', checkAuth, getMe);
+
+app.get('/posts', getAllPosts);
+app.get('/posts/:id', getOnePost);
+app.post('/posts', checkAuth, postCreateValidation, createPost);
+app.delete('/posts/:id', checkAuth, deletePost);
+app.patch('/posts/:id', checkAuth, updatePost);
 
 app.listen(PORT, (error) => {
   if (error) {
